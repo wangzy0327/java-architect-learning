ae0a929cbe363908a3a0e977c6eaaabf15d7f466
wangzy0327 <wangzy0327@qq.com>
2021-05-31 17:58:30 +0800

    add user register module

--- foodie-dev-service/src/main/java/com/imooc/service/UserService.java
+++ foodie-dev-service/src/main/java/com/imooc/service/UserService.java
@@ -1,9 +1,22 @@
 package com.imooc.service;
 
+import com.imooc.bo.UserBO;
+import com.imooc.pojo.Users;
+
 public interface UserService {
 
     /**
-     * 判断用户名是否存在
+     * 检查用户名是否存在
+     * @param username
+     * @return
      */
     boolean queryUsernameIsExist(String username);
+
+    /**
+     * 创建用户
+     * @param userBO
+     * @return
+     */
+    Users createUser(UserBO userBO);
+
 }
